0dd8e7a68981e2d488a997e5440db80c35660ba3
Lakshmi096 <lakshmirk.096@gmail.com>
2020-06-14 17:15:59 +0530

    Updates

---
+++ precache-manifest.43585a4d98061fbfbe5f2a6cc27a7493.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01d64fd62ca69fbdf715ab932ce0be46",
+    "url": "/my-resume/index.html"
+  },
+  {
+    "revision": "3b01439103eaac0dc42d",
+    "url": "/my-resume/static/css/main.90156452.chunk.css"
+  },
+  {
+    "revision": "2ff93fbb6a65d1ef01ef",
+    "url": "/my-resume/static/js/2.ed100818.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-resume/static/js/2.ed100818.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b01439103eaac0dc42d",
+    "url": "/my-resume/static/js/main.917edcf1.chunk.js"
+  },
+  {
+    "revision": "108f3eccf9ba6cf86ad4",
+    "url": "/my-resume/static/js/runtime-main.e17661cb.js"
+  },
+  {
+    "revision": "160bb2e9f78009c5f3f2b7e0a2768b52",
+    "url": "/my-resume/static/media/profile.160bb2e9.png"
+  }
+]);
